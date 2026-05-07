efcb005f312dac715cc5767dd68794921f32601b
Hannah Mezzadri <hannahmezzadri@Hannahs-MacBook-Air.local>
2026-05-07 12:52:47 -0400

    removed toggle button

--- packages/components/toggle-switch/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { ScoutToggleSwitch } from './scout-toggle-switch.js';
-export type { ToggleSwitchSize, ToggleLabelPlacement, ToggleVariant } from './types.js';
-
-import './scout-toggle-switch.js';
